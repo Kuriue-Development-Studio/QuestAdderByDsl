cc912f0f30b2b65c738ecc17b510461218d884d4
Mr byeon <124233192+AtoZuser0224@users.noreply.github.com>
2024-02-14 07:10:39 +0900

    Initial commit

---
+++ src/main/java/org/gang/rpgsystem/RpgInventorySystem.java
@@ -0,0 +1,17 @@
+package org.gang.rpgsystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RpgInventorySystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
